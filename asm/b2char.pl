eae9fc1fc9f61b076eab9decb273d22803a4ce23
yoshiki9636 <61720120+yoshiki9636@users.noreply.github.com>
2021-05-03 19:16:38 +0900

    Add comment header

--- asm/b2char.pl
+++ asm/b2char.pl
@@ -1,4 +1,13 @@
 #!/usr/bin/perl
+/*
+ * My RISC-V RV32I CPU
+ *   Code Changer from Text Dump to ASCII code for UART Sending
+ *    Perl code
+ * @auther		Yoshiki Kurokawa <yoshiki.k963@gmail.com>
+ * @copylight	2021 Yoshiki Kurokawa
+ * @license		https://opensource.org/licenses/MIT     MIT license
+ * @version		0.1
+ */
 
 use Getopt::Long;
 
