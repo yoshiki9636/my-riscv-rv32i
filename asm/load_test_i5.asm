068358137537dbf2e8bffa3c1fa99658c7d901f6
yoshiki9636 <61720120+yoshiki9636@users.noreply.github.com>
2021-08-10 22:41:27 +0900

    pass test i5

--- asm/load_test_i5.asm
+++ asm/load_test_i5.asm
@@ -20,7 +20,10 @@ ori x3, x3, 0x210 ; test value (1)
 sw x3, 0x0(x0)
 sw x3, 0x8(x0)
 lui x3, 0xfedcb ;
-ori x3, x3, 0xa98 ; test value (2)
+ori x3, x3, 0x298 ; test value (2)
+lui x4, 0x00001
+srli x4, x4, 1;
+or x3, x3, x4
 sw x3, 0x4(x0)
 sw x3, 0xc(x0)
 ; test lb offset 0
@@ -36,8 +39,7 @@ sb x1, 0x0(x2) ; set LED
 :fail_test2
 ori x4, x0, 0x005
 lb x5, 0x0(x4)
-lui x6, 0xfffff
-ori x6, x6, 0xfba
+ori x6, x0, 0xfba
 bne x5, x6, fail_test2
 ; next value
 addi x1, x0, 5 ; LED value
@@ -46,8 +48,7 @@ sb x1, 0x0(x2) ; set LED
 :fail_test3
 ori x4, x0, 0x006
 lb x5, 0x0(x4)
-lui x6, 0xfffff
-ori x6, x6, 0xfdc
+ori x6, x0, 0xfdc
 bne x5, x6, fail_test3
 ; next value
 addi x1, x0, 4 ; LED value
@@ -66,7 +67,10 @@ sb x1, 0x0(x2) ; set LED
 ori x4, x0, 0x004
 lh x5, 0x0(x4)
 lui x6, 0xffffb
-ori x6, x6, 0xa87
+ori x6, x6, 0x298
+lui x7, 0x00001
+srli x7, x7, 1
+or x6, x6, x7
 bne x5, x6, fail_test5
 ; next value
 addi x1, x0, 2 ; LED value
@@ -76,7 +80,7 @@ sb x1, 0x0(x2) ; set LED
 ori x4, x0, 0x002
 lh x5, 0x0(x4)
 lui x6, 0x00007
-ori x6, x6, 0x754
+ori x6, x6, 0x654
 bne x5, x6, fail_test6
 ; next value
 addi x1, x0, 1 ; LED value
@@ -86,7 +90,10 @@ sb x1, 0x0(x2) ; set LED
 ori x4, x0, 0x004
 lw x5, 0x0(x4)
 lui x6, 0xfedcb
-ori x6, x6, 0xa98
+ori x6, x6, 0x298
+lui x7, 0x00001
+srli x7, x7, 1
+or x6, x6, x7
 bne x5, x6, fail_test7
 addi x1, x0, 0 ; LED value
 sb x1, 0x0(x2) ; set LED
@@ -96,7 +103,8 @@ sb x1, 0x0(x2) ; set LED
 ; test finished
 nop
 nop
-lui x2, 01000 ; loop max
+;lui x2, 01000 ; loop max
+ori x2, x0, 10
 and x3, x0, x3 ; LED value
 and x4, x0, x4 ;
 lui x4, 0xc0000 ; LED address
