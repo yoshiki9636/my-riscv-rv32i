9b001fe3ce55b262b984ba331e93308f54e99f3e
yoshiki9636 <61720120+yoshiki9636@users.noreply.github.com>
2021-08-10 18:10:12 +0900

    pass test i4

--- asm/alu_test_i4.asm
+++ asm/alu_test_i4.asm
@@ -16,33 +16,33 @@ lui x2, 0xc0000 ; LED address
 sb x1, 0x0(x2) ; set LED
 ; test xor
 :fail_test1
-lui x3, 0xaa55c
-ori x3, x3, 0xc33
+lui x3, 0x55aa3
+ori x3, x3, 0x3cc
 lui x4, 0xa5a5c
 ori x4, x4, 0x3c3
 xor x3, x3, x4
-lui x4, 0x0ff00
-ori x4, x4, 0xff0
+lui x4, 0xf00ff
+ori x4, x4, 0x00f
 bne x4, x3, fail_test1
 ; next value
 addi x1, x0, 6 ; LED value
 sb x1, 0x0(x2) ; set LED
 ; test srl
 :fail_test2
-lui x5, 0xaa55c
-ori x5, x5, 0xc33
+lui x5, 0x55aa3
+ori x5, x5, 0x3cc
 ori x6, x0, 0xfe3
 srl x5, x5, x6
-lui x6, 0x154ab
-ori x6, x6, 0x986
+lui x6, 0x0ab54
+ori x6, x6, 0x679
 bne x5, x6, fail_test2
 ; next srl (2)
 addi x1, x0, 5 ; LED value
 sb x1, 0x0(x2) ; set LED
 :fail_test3
-lui x5, 0xaa55c
-ori x5, x5, 0xc33
-ori x6, x0, 0xfff
+lui x5, 0x55aa3
+ori x5, x5, 0x3cc
+ori x6, x0, 0xffe
 srl x5, x5, x6
 ori x6, x0, 1
 bne x5, x6, fail_test3
@@ -51,24 +51,24 @@ addi x1, x0, 4 ; LED value
 sb x1, 0x0(x2) ; set LED
 ; test sra
 :fail_test4
-lui x5, 0xaa55c
-ori x5, x5, 0xc33
-ori x6, x0, 0xfe1
+lui x5, 0xf5aa3
+ori x5, x5, 0x3cc
+ori x6, x0, 0xfe7
 sra x5, x5, x6
-lui x6, 0x552ae
-ori x6, x6, 0x619
+lui x6, 0xffeb5
+ori x6, x6, 0x467
 bne x5, x6, fail_test4
 ; next value
 addi x1, x0, 3 ; LED value
 sb x1, 0x0(x2) ; set LED
 ; test sra (2)
 :fail_test5
-lui x5, 0xaa55c
-ori x5, x5, 0xc33
-ori x6, x0, 0xfff
+lui x5, 0x55aa3
+ori x5, x5, 0x3cc
+ori x6, x0, 0xfe3
 sra x5, x5, x6
-lui x6, 0x54ab9
-ori x6, x6, 0x866
+lui x6, 0x0ab54
+ori x6, x6, 0x679
 bne x5, x6, fail_test5
 ; next value
 addi x1, x0, 2 ; LED value
@@ -78,9 +78,11 @@ sb x1, 0x0(x2) ; set LED
 lui x5, 0x76543
 ori x5, x5, 0x210
 lui x6, 0x89abc
-ori x6, x6, 0xdef
+ori x6, x6, 0x5ef
+lui x7, 0x00001
+srli x7, x7, 1
+or x6, x6, x7
 or x5, x5, x6
-lui x6, 0xfffff
 ori x6, x6, 0xfff
 bne x5, x6, fail_test6
 ; next value
@@ -90,19 +92,19 @@ sb x1, 0x0(x2) ; set LED
 :fail_test7
 lui x5, 0x76543
 ori x5, x5, 0x210
-lui x6, 0xfffff
 ori x6, x6, 0xfff
 and x5, x5, x6
-lui x6, 0x76543
-ori x6, x6, 0x210
-bne x5, x6, fail_test7
+lui x7, 0x76543
+ori x7, x7, 0x210
+bne x5, x7, fail_test7
 ; next value
 addi x1, x0, 0 ; LED value
 sb x1, 0x0(x2) ; set LED
 ; test finished
 nop
 nop
-lui x2, 01000 ; loop max
+;lui x2, 01000 ; loop max
+ori x2, x0, 10 ; loop max
 and x3, x0, x3 ; LED value
 and x4, x0, x4 ;
 lui x4, 0xc0000 ; LED address
