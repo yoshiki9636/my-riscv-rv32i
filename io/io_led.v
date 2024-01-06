e9ce15c30768b6d060b5f02cd3ea214f5e4d28e6
yoshiki9636 <61720120+yoshiki9636@users.noreply.github.com>
2024-01-06 11:25:10 +0900

    support 4 LEDs for Arty A7

--- io/io_led.v
+++ io/io_led.v
@@ -19,11 +19,14 @@ module io_led(
     input [15:2] dma_io_radr,
     input [31:0] dma_io_rdata_in,
     output [31:0] dma_io_rdata,
-	output [2:0] rgb_led
+	output [2:0] rgb_led,
+	output [2:0] rgb_led1,
+	output [2:0] rgb_led2,
+	output [2:0] rgb_led3
 
 	);
 
-reg [2:0] led_value;
+reg [11:0] led_value;
 
 // decode :: adr 0x0 : LED values
 `define SYS_LED_IO 14'h3F80
@@ -34,12 +37,15 @@ wire re_led_value = (dma_io_radr == `SYS_LED_IO);
 
 always @ (posedge clk or negedge rst_n) begin
     if (~rst_n)
-        led_value <= 3'd0 ;
+        led_value <= 12'd0 ;
 	else if ( we_led_value )
-		led_value <= dma_io_wdata[2:0];
+		led_value <= { dma_io_wdata[14:12],dma_io_wdata[10:8], dma_io_wdata[6:4], dma_io_wdata[2:0]};
 end
 
-assign dma_io_rdata = re_led_value ? { 29'd0, led_value } : dma_io_rdata_in;
-assign rgb_led = led_value;
+assign dma_io_rdata = re_led_value ? { 17'd0, led_value[11:9], 1'b0, led_value[8:6], 1'b0, led_value[5:3], 1'b0, led_value[2:0] } : dma_io_rdata_in;
+assign rgb_led = led_value[2:0];
+assign rgb_led1 = led_value[5:3];
+assign rgb_led2 = led_value[8:6];
+assign rgb_led3 = led_value[11:9];
 
 endmodule
