a9c12b2971d31b7118244b385b0724b09aa201a2
yoshiki9636 <61720120+yoshiki9636@users.noreply.github.com>
2024-11-23 22:12:30 +0900

    support soft-float

--- ctest/print_test3.c
+++ ctest/print_test3.c
@@ -4,38 +4,36 @@
 #define LP 1000000
 #define LP2 200
 void pass();
-//void fail();
+void fail();
 void wait();
-//int int_print( char* cbuf, int value, int type );
-int float_print( char* cbuf, float value, int digit );
+int int_print( char* cbuf, int value, int type );
+int double_print( char* cbuf, double value, int digit );
 void uprint( char* buf, int length );
 
 int main() {
-	//char cbuf[15] = "Hello World!!\n";
+	char cbuf[13] = "Hello World!!";
 	char cbuf2[32];
+	int a = -12345;
+	double b = -876543.21;
 
-	//int a = -12345;
-	float b = 543.21;
+	// print Hello world
+	uprint( cbuf, 13 );
 
-/*
+	// pritn a
 	int length = int_print( cbuf2, a, 0 );
 	if (length > 10) {
 		fail();
 	}
 	uprint( cbuf2, length );
-*/
 
-	int length = float_print( cbuf2, b, 8 );
-	//if (length > 10) {
-		//fail();
-	//}
+	// pritn b
+	length = double_print( cbuf2, b, 9 );
+
 	uprint( cbuf2, length );
 	pass();
 	return 0;
-
 }
 
-/*
 int int_print( char* cbuf, int value, int type ) {
 	// type 0 : digit  1:hex
 	unsigned char buf[32];
@@ -66,59 +64,66 @@ int int_print( char* cbuf, int value, int type ) {
 	}
 	return ofs;	
 }
-*/
 
-int float_print( char* cbuf, float value, int digit ) {
-    unsigned int* led = (unsigned int*)0xc000fe00;
+int double_print( char* cbuf, double value, int digit ) {
 	// type 0 : digit  1:hex
 	unsigned char buf[32];
 
-	int ofs = 0;
 	int cntr = 0;
 	
 	if (value < 0) {
-		cbuf[ofs++] = 0x2d;
+		buf[cntr++] = 0xfe; // for minus
 		value = -value;
 	}
-	float mug = 1.0;
+	double mug = 1.0;
 	while(value > mug) {
 		mug *= 10.0;
 	}
 	mug /= 10.0;
 	for(int i = 0; i < digit; i++) {	
-		float a = value / mug;
-		buf[cntr++] = (unsigned char)a;
-		value = value - a * mug;
-		if (mug == 1) {
+		unsigned char a =(unsigned char)(value / mug);
+		buf[cntr++] = a;
+		value = value - (double)a * mug;
+		if (mug == 1.0) {
 			buf[cntr++] = 0xff; // for preiod
 		}
 		mug /= 10.0;
-		*led = i;
+	}
+	if (mug >= 1.0) {
+		while(mug >= 1.0) {
+			unsigned char a =(unsigned char)(value / mug);
+			buf[cntr++] = a;
+			value = value - (double)a * mug;
+			mug /= 10.0;
+		}
 	}
 	for(int i = 0; i < cntr; i++) {	
 		if (buf[i] == 0xff) {
-			cbuf[ofs++] = 0x2e;
+			cbuf[i] = 0x2e;
+		}
+		else if (buf[i] == 0xfe) {
+			cbuf[i] = 0x2d;
 		}
 		else {
-			cbuf[ofs++] = buf[i] + 0x30;
+			cbuf[i] = buf[i] + 0x30;
 		}
-		*led = i;
 	}	
-	return ofs;	
+	return cntr;	
 }
-
 
 void uprint( char* buf, int length ) {
     unsigned int* led = (unsigned int*)0xc000fe00;
     unsigned int* uart_out = (unsigned int*)0xc000fc00;
     unsigned int* uart_status = (unsigned int*)0xc000fc04;
 
-	for (int i = 0; i < length + 1; i++) {
+	for (int i = 0; i < length + 3; i++) {
 		unsigned int flg = 1;
 		while(flg == 1) {
 			flg = *uart_status;
 		}
-		*uart_out = (i == length) ? 0 : buf[i];
+		*uart_out = (i == length+2) ? 0 :
+		            (i == length+1) ? 0x0a :
+		            (i == length) ? 0x0d : buf[i];
 		*led = i;
 	}
 	//return 0;
@@ -136,7 +141,6 @@ void pass() {
     }
 }
 
-/*
 void fail() {
     unsigned int* led = (unsigned int*)0xc000fe00;
     unsigned int val;
@@ -148,7 +152,6 @@ void fail() {
 		*led = val & 0x1111;
     }
 }
-*/
 
 void wait() {
     unsigned int timer,timer2;
